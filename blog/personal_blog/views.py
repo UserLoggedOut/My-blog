a9310b6800f1aefe45d7457b57884da51e4ed901
吴超 <15835767493@163.com>
2019-12-17 21:27:47 +0800

    添加主\子路由，编写index视图函数

--- blog/personal_blog/views.py
+++ blog/personal_blog/views.py
@@ -1,3 +1,8 @@
+from django import http
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class IndexView(View):  # 显示主页
+    def get(self, request):
+        return http.HttpResponse('欢迎访问我的博客')
