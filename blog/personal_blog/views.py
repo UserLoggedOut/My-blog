4f2f23f132eb29887c190ee48879c0f84ef50667
吴超 <15835767493@163.com>
2019-12-17 22:26:14 +0800

    添加静态文件，修改首页模板

--- blog/personal_blog/views.py
+++ blog/personal_blog/views.py
@@ -2,10 +2,10 @@ from django import http
 from django.shortcuts import render
 from django.views import View
 
+from personal_blog.models import Post
+
 
 class IndexView(View):  # 显示主页
     def get(self, request):
-        return render(request, 'personal_blog/index.html', context={
-            'title': '我的博客首页',
-            'welcome': '欢迎访问我的博客首页'
-        })
+        post_list = Post.objects.all().order_by('-created_time')
+        return render(request, 'personal_blog/index.html', context={'post_list': post_list})
