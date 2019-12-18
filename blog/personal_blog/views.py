0a14e836ffff2a7b67418c679e669f54d51f1f4e
吴超 <15835767493@163.com>
2019-12-18 13:59:55 +0800

    处理空目录

--- blog/personal_blog/views.py
+++ blog/personal_blog/views.py
@@ -1,3 +1,5 @@
+import re
+
 import markdown
 from django.shortcuts import render, get_object_or_404
 from django.views import View
@@ -21,5 +23,7 @@ class DetailView(View):  # 显示详情页
             'markdown.extensions.toc'
         ])
         post.body = md.convert(post.body)
-        post.toc = md.toc  #
+        m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>',
+                      md.toc, re.S)  # 如果文章没有任何标题元素时，MD就提取不出目录结构，post.toc就是一个空的<div>标签
+        post.toc = m.group(1) if m is not None else ''  # 使用三目运算符判断是否有数据
         return render(request, 'personal_blog/detail.html', context={'post': post})
